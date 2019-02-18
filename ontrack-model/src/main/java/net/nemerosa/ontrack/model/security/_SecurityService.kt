123f424602f2a4670c592554756ba8a2c4d10f7b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-02-18 10:29:14 +0100

    #596 Correction after merge

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/_SecurityService.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/_SecurityService.kt
@@ -2,9 +2,6 @@ package net.nemerosa.ontrack.model.security
 
 import java.util.function.Supplier
 
-fun <T> SecurityService.callAsAdmin(call: () -> T): T =
-        asAdmin(Supplier { call() })
-
 /**
  * Given a [function][call] which returns an object, returns _another_ function
  * which wraps it into a secure call. The returned function can be called
