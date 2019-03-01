98339e866d9f15b79a8d6f5e20fcb72702c3de3b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-03-01 17:33:23 +0100

    #650 Package versions acceptance test

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCDSLBuildPackageVersions.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCDSLBuildPackageVersions.groovy
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.acceptance
 
 import net.nemerosa.ontrack.acceptance.support.AcceptanceTestSuite
+import net.nemerosa.ontrack.dsl.Build
 import org.junit.Test
 
 import static net.nemerosa.ontrack.test.TestUtils.uid
@@ -59,6 +60,63 @@ class ACCDSLBuildPackageVersions extends AbstractACCDSL {
 
     }
 
-    // TODO Reference project using labels
+
+    @Test
+    void 'Uploading package versions for a build using a properties file referring to labels'() {
+        // Project to use as reference
+        def refName = uid('P')
+        ontrack.project(refName) {
+            setPackageIds(
+                maven: [ "net.nemerosa.ontrack:ontrack-model" ],
+                docker: [ "nemerosa/ontrack" ],
+            )
+
+            // Using labels for identification
+            config {
+                buildLinkDisplayOptions(true)
+            }
+
+            branch("master") {
+                build("3.38.5-12334567") {
+                    // Label to refer to
+                    config {
+                        label "3.38.5"
+                    }
+                }
+            }
+        }
+        // Reference build
+        def ref = ontrack.build(refName, "master", "3.38.5-12334567")
+
+        // Parent project & build
+        def parentName = uid("P")
+        ontrack.project(parentName) {
+            branch("master") {
+                build("1.0.0") {
+                    uploadPackageVersions ACCDSLBuildPackageVersions.class.getResource("/build/versions/upload.properties"), "application/properties"
+                }
+            }
+        }
+
+        // Gets the parent build and its versions
+        def versions = ontrack.build(parentName, "master", "1.0.0").packageVersions
+
+        def ontrackDep = versions.resources.get(0)
+        assert ontrackDep.packageVersion.packageId.type.name == "Maven"
+        assert ontrackDep.packageVersion.packageId.id == "net.nemerosa.ontrack:ontrack-model"
+        assert ontrackDep.packageVersion.version == "3.38.5"
+        assert ontrackDep.target != null: "Ref build has been found"
+        assert ontrackDep.target.id == ref.id
+        assert ontrackDep.target.name == "3.38.5-12334567"
+        def refBuild = new Build(ontrack, ontrackDep.target)
+        assert refBuild.config.label == "3.38.5"
+
+        def springDep = versions.resources.get(1)
+        assert springDep.packageVersion.packageId.type.name == "Maven"
+        assert springDep.packageVersion.packageId.id == "org.springframework.boot:spring-boot-gradle-plugin"
+        assert springDep.packageVersion.version == "1.5.14.RELEASE"
+        assert springDep.target == null
+
+    }
 
 }
