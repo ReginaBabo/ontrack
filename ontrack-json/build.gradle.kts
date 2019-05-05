778cdb5a87098585554b81c35422b7b52cb028b5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-05 21:24:12 +0200

    #663 [gradle-kotlin] `json` module

---
+++ ontrack-json/build.gradle.kts
@@ -0,0 +1,8 @@
+description = "JSON utilities."
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-core")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.apache.commons:commons-lang3")
+}
