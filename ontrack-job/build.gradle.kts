4cb89814b64d4ade8668280ceca144685f21c1ee
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-05 21:28:49 +0200

    #663 [gradle-kotlin] `job` module

---
+++ ontrack-job/build.gradle.kts
@@ -0,0 +1,14 @@
+description = "Abstract management of identified jobs."
+
+dependencies {
+    implementation(project(":ontrack-common"))
+    implementation("org.springframework:spring-context")
+    implementation("org.slf4j:slf4j-api")
+    implementation("io.micrometer:micrometer-core")
+    implementation("org.apache.commons:commons-lang3")
+
+    testImplementation(project(":ontrack-test-utils"))
+    testImplementation("org.apache.commons:commons-math3")
+    testImplementation("com.google.guava:guava")
+    testRuntimeOnly("org.slf4j:slf4j-log4j12")
+}
