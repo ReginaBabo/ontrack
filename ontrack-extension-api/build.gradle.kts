b153eb5a3c13a0397310425eb41a5ce7de8847d0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-06 22:02:16 +0200

    #663 [gradle-kotlin] `extension-api` module with tests

--- ontrack-extension-api/build.gradle.kts
+++ ontrack-extension-api/build.gradle.kts
@@ -1,24 +1,14 @@
 dependencies {
     api(project(":ontrack-model"))
-    
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.apache.commons:commons-lang3")
     implementation("com.google.guava:guava")
 }
 
-// FIXME Test artifact export
-//configurations {
-//    testArtifacts
-//}
-//
-//task testJar(type: Jar) {
-//    classifier = "tests"
-//    from sourceSets.test.output
-//}
-//
-//assemble.dependsOn testJar
-//
-//artifacts {
-//    archives testJar
-//    testArtifacts testJar
-//}
+tasks.register<Jar>("testJar") {
+    classifier = "tests"
+    from(sourceSets["test"].allJava)
+}
+
+tasks["assemble"].dependsOn("testJar")
