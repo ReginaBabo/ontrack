f0cdaa6f22fbd7a9fb97be29af4f65c401d60f95
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-06 22:08:33 +0200

    #663 [gradle-kotlin] `repository-support` module (WIP)

---
+++ ontrack-repository-support/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    api(project(":ontrack-model"))
+    api("org.springframework:spring-jdbc")
+
+    implementation("org.springframework:spring-context")
+    implementation("org.apache.commons:commons-lang3")
+}
