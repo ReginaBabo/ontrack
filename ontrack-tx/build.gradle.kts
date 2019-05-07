166e5417169df04c245536e3dfa86a6f881490ec
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-07 16:38:44 +0200

    #663 [gradle-kotlin] `tx` module

---
+++ ontrack-tx/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    implementation("com.google.guava:guava")
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-context")
+}
