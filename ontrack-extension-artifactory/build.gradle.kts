a5f55b442d8fefd7ebb2f3dbc71bebb92bf3d9f1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-10 17:09:25 +0200

    #663 [gradle-kotlin] `extension-artifactory` module

--- ontrack-extension-artifactory/build.gradle.kts
+++ ontrack-extension-artifactory/build.gradle.kts
@@ -1,12 +1,16 @@
 import net.nemerosa.ontrack.gradle.extension.OntrackExtensionPlugin
 
-apply plugin: OntrackExtensionPlugin
+plugins {
+    `java-library`
+}
+
+apply<OntrackExtensionPlugin>()
 
 dependencies {
-    compile project(':ontrack-extension-support')
+    implementation(project(":ontrack-extension-support"))
     implementation(project(":ontrack-ui-support"))
     implementation(project(":ontrack-client"))
     implementation("org.apache.commons:commons-lang3")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    compile 'org.springframework:spring-tx'
+    implementation("org.springframework:spring-tx")
 }
