7d5110b2640a043e0c8a3f475d83a66c99c75f91
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-17 14:36:40 +0200

    #663 [gradle-kotlin] `extension-ldap` module

---
+++ ontrack-extension-ldap/build.gradle.kts
@@ -0,0 +1,22 @@
+import net.nemerosa.ontrack.gradle.extension.OntrackExtensionPlugin
+
+plugins {
+    `java-library`
+    groovy
+}
+
+apply<OntrackExtensionPlugin>()
+
+dependencies {
+    implementation(project(":ontrack-extension-support"))
+    implementation("org.apache.commons:commons-lang3")
+    implementation("org.springframework.security:spring-security-ldap")
+    implementation(project(":ontrack-ui-support"))
+    implementation("javax.validation:validation-api")
+
+    testImplementation(project(":ontrack-it-utils"))
+    testImplementation("org.codehaus.groovy:groovy-all")
+
+    testRuntimeOnly(project(":ontrack-service"))
+    testRuntimeOnly(project(":ontrack-repository-impl"))
+}
