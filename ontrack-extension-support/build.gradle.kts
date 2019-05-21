d7cc572530a0a0e03bb76de63ee008f7bda6e159
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-05-21 20:53:59 +0200

    #663 [gradle-kotlin] Correction of dependencies

--- ontrack-extension-support/build.gradle.kts
+++ ontrack-extension-support/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 
 dependencies {
     api(project(":ontrack-extension-api"))
+    api("org.slf4j:slf4j-api")
     // Extensions do need Spring annotations
     api("org.springframework:spring-context")
 
     implementation(project(":ontrack-client"))
     implementation(project(":ontrack-ui-support"))
     implementation("org.apache.commons:commons-lang3")
-    implementation("org.slf4j:slf4j-api")
 
     // Make sure the following libraries are available for the extension when they need them
     runtimeOnly(project(":ontrack-git"))
--- ontrack-it-utils/build.gradle.kts
+++ ontrack-it-utils/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 
 dependencies {
     api(project(":ontrack-model"))
-    
-    implementation(project(":ontrack-test-utils"))
+    api(project(":ontrack-test-utils"))
+    api("org.springframework:spring-test")
+    api("org.springframework:spring-jdbc")
+
     implementation(project(":ontrack-extension-support"))
     implementation(project(":ontrack-ui-support"))
     implementation("com.google.guava:guava")
     implementation("org.springframework:spring-context")
-    implementation("org.springframework:spring-jdbc")
-    implementation("org.springframework:spring-test")
     implementation("org.springframework.security:spring-security-core")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.slf4j:slf4j-api")
