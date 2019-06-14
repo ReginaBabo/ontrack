78d507881c0159df2f7e78f0f87a6f86725fb321
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-06-14 21:11:14 +0200

    #663 [gradle-kotlin] `extension-jenkins` module

--- ontrack-extension-jenkins/build.gradle.kts
+++ ontrack-extension-jenkins/build.gradle.kts
@@ -1,16 +1,21 @@
 import net.nemerosa.ontrack.gradle.extension.OntrackExtensionPlugin
 
-apply plugin: OntrackExtensionPlugin
-apply plugin: 'groovy'
+plugins {
+    `java-library`
+    groovy
+}
+
+apply<OntrackExtensionPlugin>()
 
 dependencies {
-    compile project(':ontrack-extension-support')
+    implementation(project(":ontrack-extension-support"))
     implementation(project(":ontrack-ui-support"))
     implementation(project(":ontrack-client"))
     implementation("org.apache.commons:commons-lang3")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    compile 'org.springframework:spring-tx'
+    implementation("org.springframework:spring-tx")
 
-    testCompile 'org.codehaus.groovy:groovy-all'
-    testCompile project(':ontrack-test-utils')
+    testImplementation("org.codehaus.groovy:groovy-all")
+    testImplementation(project(":ontrack-test-utils"))
+
 }
