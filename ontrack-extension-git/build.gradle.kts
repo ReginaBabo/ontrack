44cd9475a2f9cc0b3636227be0faa43f5b6447f1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-06-15 17:16:17 +0200

    #663 [gradle-kotlin] `extension-git` module

--- ontrack-extension-git/build.gradle.kts
+++ ontrack-extension-git/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     api(project(":ontrack-git"))
     api(project(":ontrack-json"))
     api(project(":ontrack-extension-scm"))
+    api("org.springframework:spring-tx")
 
     implementation(project(":ontrack-ui-graphql"))
     implementation(project(":ontrack-ui-support"))
@@ -18,7 +19,6 @@ dependencies {
     implementation(project(":ontrack-repository-support"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.google.guava:guava")
-    implementation("org.springframework:spring-tx")
     implementation("org.apache.commons:commons-lang3")
     implementation("commons-io:commons-io")
 
