5e65333b53be3dbf1682c69ac33f0998014f4538
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-06-15 17:07:07 +0200

    #663 [gradle-kotlin] `extension-git` module

---
+++ ontrack-extension-git/build.gradle.kts
@@ -0,0 +1,33 @@
+import net.nemerosa.ontrack.gradle.extension.OntrackExtensionPlugin
+
+plugins {
+    `java-library`
+    groovy
+}
+
+apply<OntrackExtensionPlugin>()
+
+dependencies {
+    api(project(":ontrack-git"))
+    api(project(":ontrack-json"))
+    api(project(":ontrack-extension-scm"))
+
+    implementation(project(":ontrack-ui-graphql"))
+    implementation(project(":ontrack-ui-support"))
+    implementation(project(":ontrack-tx"))
+    implementation(project(":ontrack-repository-support"))
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.google.guava:guava")
+    implementation("org.springframework:spring-tx")
+    implementation("org.apache.commons:commons-lang3")
+    implementation("commons-io:commons-io")
+
+    testImplementation(project(":ontrack-it-utils"))
+    testImplementation(project(path = ":ontrack-extension-api", configuration = "test"))
+    testImplementation(project(path = ":ontrack-extension-issues", configuration = "tests"))
+    testImplementation(project(path = ":ontrack-ui-graphql", configuration = "tests"))
+    testImplementation("org.codehaus.groovy:groovy-all")
+    testRuntimeOnly(project(":ontrack-service"))
+    testRuntimeOnly(project(":ontrack-repository-impl"))
+    testRuntimeOnly("org.springframework.boot:spring-boot-starter-web")
+}
