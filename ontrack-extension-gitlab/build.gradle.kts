8552725bcc8912f400ccfd60daabc3b2255d4d4a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-06-15 21:00:01 +0200

    #663 `gitlab` extension

---
+++ ontrack-extension-gitlab/build.gradle.kts
@@ -0,0 +1,23 @@
+import net.nemerosa.ontrack.gradle.extension.OntrackExtensionPlugin
+
+plugins {
+    `java-library`
+}
+
+apply<OntrackExtensionPlugin>()
+
+dependencies {
+    implementation(project(":ontrack-extension-git"))
+    implementation("org.apache.commons:commons-lang3")
+    implementation("org.gitlab:java-gitlab-api:1.2.7")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.google.guava:guava")
+
+    testImplementation(project(":ontrack-test-utils"))
+    testImplementation(project(":ontrack-it-utils"))
+    testImplementation("org.springframework.boot:spring-boot-starter-actuator")
+    testImplementation(project(path = ":ontrack-extension-issues", configuration = "tests"))
+
+    testRuntimeOnly(project(":ontrack-service"))
+    testRuntimeOnly(project(":ontrack-repository-impl"))
+}
