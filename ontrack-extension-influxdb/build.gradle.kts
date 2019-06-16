14dbd8542ab8e79b446c8b7c89c5459ac4f6b325
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-06-16 16:55:11 +0200

    #663 `influxdb` extension

---
+++ ontrack-extension-influxdb/build.gradle.kts
@@ -0,0 +1,22 @@
+import net.nemerosa.ontrack.gradle.extension.OntrackExtensionPlugin
+
+plugins {
+    `java-library`
+}
+
+apply<OntrackExtensionPlugin>()
+
+dependencies {
+    api("org.influxdb:influxdb-java")
+    api(project(":ontrack-extension-support"))
+
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    testImplementation(project(":ontrack-it-utils"))
+    testImplementation("org.codehaus.groovy:groovy-all")
+    testImplementation(project(path = ":ontrack-extension-api", configuration = "tests"))
+
+    testRuntimeOnly(project(":ontrack-service"))
+    testRuntimeOnly(project(":ontrack-repository-impl"))
+}
