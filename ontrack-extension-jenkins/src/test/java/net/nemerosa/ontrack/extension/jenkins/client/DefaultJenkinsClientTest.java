f8eb2112b658349c0c7bf6292e7144326862f4df
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-07-08 16:04:03 +0200

    #663 Correction of tests

--- ontrack-extension-jenkins/src/test/java/net/nemerosa/ontrack/extension/jenkins/client/DefaultJenkinsClientTest.java
+++ ontrack-extension-jenkins/src/test/java/net/nemerosa/ontrack/extension/jenkins/client/DefaultJenkinsClientTest.java
@@ -7,8 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,7 +19,7 @@ public class DefaultJenkinsClientTest {
     @Before
     public void before() {
         OTHttpClient httpClient = mock(OTHttpClient.class);
-        when(httpClient.getUrl(anyString(), any(String[].class))).thenAnswer(invocation -> {
+        when(httpClient.getUrl(anyString(), any())).thenAnswer(invocation -> {
             String path = (String) invocation.getArguments()[0];
             String parameters = (String) invocation.getArguments()[1];
             return String.format(
