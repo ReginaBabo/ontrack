93e67131fac07ca53fddc4c6744510b3cf46108f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-07-09 21:34:44 +0200

    #663 `buildSrc`

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    groovy
+}
+
+repositories {
+    maven {
+        url = uri("https://plugins.gradle.org/m2/")
+    }
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation("gradle.plugin.com.liferay:gradle-plugins-node:4.6.18")
+    compile("net.nemerosa.ontrack:ontrack-dsl:2.23.2") {
+        exclude(module = "groovy-all")
+    }
+}
