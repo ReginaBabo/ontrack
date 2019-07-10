d4054676db59d0d1a9f20927d0461841e2fb0a52
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-07-10 20:38:05 +0200

    #663 Extension support must expose some API

--- ontrack-extension-support/build.gradle.kts
+++ ontrack-extension-support/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 dependencies {
     api(project(":ontrack-extension-api"))
+    api(project(":ontrack-ui-support"))
     api("org.slf4j:slf4j-api")
     // Extensions do need Spring annotations
     api("org.springframework:spring-context")
 
     implementation(project(":ontrack-client"))
-    implementation(project(":ontrack-ui-support"))
     implementation("org.apache.commons:commons-lang3")
 
     // Make sure the following libraries are available for the extension when they need them
--- ontrack-ui-support/build.gradle.kts
+++ ontrack-ui-support/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     api(project(":ontrack-model"))
     api(project(":ontrack-extension-api"))
     api("org.springframework:spring-webmvc")
+    api("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.apache.commons:commons-lang3")
 
     testImplementation(project(path = ":ontrack-model", configuration = "tests"))
