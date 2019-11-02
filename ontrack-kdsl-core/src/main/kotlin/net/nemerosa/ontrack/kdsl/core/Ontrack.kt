45624d3eb1b8d9592236334f51f33ec742973b26
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-02 21:29:31 +0100

    [kdsl] Ontrack root object

--- ontrack-kdsl-core/src/main/kotlin/net/nemerosa/ontrack/kdsl/core/Ontrack.kt
+++ ontrack-kdsl-core/src/main/kotlin/net/nemerosa/ontrack/kdsl/core/Ontrack.kt
@@ -1,8 +1,9 @@
 package net.nemerosa.ontrack.kdsl.core
 
+import net.nemerosa.ontrack.kdsl.client.OntrackConnector
+
 /**
  * Root object of the Ontrack model. Typically extended by the model and
  * extensions for the Kotlin DSL.
  */
-interface Ontrack {
-}
+class Ontrack(ontrackConnector: OntrackConnector) : Connector(ontrackConnector)
