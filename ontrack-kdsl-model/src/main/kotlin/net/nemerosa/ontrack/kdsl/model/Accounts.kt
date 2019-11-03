3eae02b314bdca0b76d8f18d8f7ac2467858f625
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-03 09:15:13 +0100

    [kdsl] More signatures

--- ontrack-kdsl-model/src/main/kotlin/net/nemerosa/ontrack/kdsl/model/Accounts.kt
+++ ontrack-kdsl-model/src/main/kotlin/net/nemerosa/ontrack/kdsl/model/Accounts.kt
@@ -16,6 +16,24 @@ class Accounts : Connector() {
     ) {
         TODO("Gets or creates the group")
     }
+
+    /**
+     * Creating or updating an account
+     *
+     * @param name Unique name for the account
+     * @param fullName Display name for the account (defaults to [name])
+     * @param email Email for the account (defaults to "[name]@test.com)
+     * @param password Password the account
+     * @param groupNames Groups the account belongs to
+     */
+    fun account(
+            name: String,
+            fullName: String?,
+            email: String?,
+            password: String = "",
+            groupNames: List<String> = emptyList()) {
+        TODO("Creating or updating an account")
+    }
 }
 
 /**
---
+++ ontrack-kdsl-model/src/main/kotlin/net/nemerosa/ontrack/kdsl/model/Permissions.kt
@@ -0,0 +1,14 @@
+package net.nemerosa.ontrack.kdsl.model
+
+/**
+ * Sets global permissions on an account group
+ *
+ * @param groupName Name of the group
+ * @param role Role to associate to the group
+ */
+fun Accounts.setAccountGroupGlobalPermission(
+        groupName: String,
+        role: String
+) {
+    TODO("Sets global permissions on an account group")
+}
