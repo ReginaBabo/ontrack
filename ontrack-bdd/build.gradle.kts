272664d3aaca09191b15e7c75a1b9183c8bd673f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-05 20:53:00 +0100

    [bdd] Spring Boot packaging

--- ontrack-bdd/build.gradle.kts
+++ ontrack-bdd/build.gradle.kts
@@ -1,6 +1,54 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+description = "BDD scenarios for Ontrack E2E tests, including for core extensions."
+
+apply(plugin = "org.springframework.boot")
+
 dependencies {
     testImplementation(project(":ontrack-bdd-engine"))
     testImplementation(project(":ontrack-bdd-definitions"))
 
     testImplementation(project(":ontrack-extension-stale", "bddConfig"))
+}
+/**
+ * Packaging
+ */
+
+val bootJar = tasks.getByName<BootJar>("bootJar") {
+    bootInf {
+        from(sourceSets["test"].output)
+        into("classes")
+    }
+    classpath(configurations.named("testRuntimeClasspath"))
+    mainClassName = "net.nemerosa.ontrack.bdd.BDDApp"
+}
+
+val normaliseJar by tasks.registering(Copy::class) {
+    dependsOn(bootJar)
+    from("$buildDir/libs/")
+    include("ontrack-bdd-$version.jar")
+    into("$buildDir/libs/")
+    rename("ontrack-bdd-$version.jar", "ontrack-bdd.jar")
+}
+
+tasks.named("assemble") {
+    dependsOn(normaliseJar)
+}
+
+rootProject.tasks.named<Zip>("publicationPackage") {
+    from(bootJar)
+}
+
+// Disable unit tests (none in this project)
+
+tasks.named<Test>("test") {
+    enabled = false
+}
+
+configure<PublishingExtension> {
+    publications {
+        named<MavenPublication>("mavenCustom") {
+            setArtifacts(listOf(bootJar))
+        }
+    }
 }
