ea36a3c405fc158f9d832e5c12d8f3ae2ea9809e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-09 21:00:03 +0100

    [bdd] Serenity options

--- ontrack-bdd/src/main/docker/bdd.sh
+++ ontrack-bdd/src/main/docker/bdd.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 exec java \
     ${JAVA_OPTIONS} \
-    -Dwebdriver.base.url=${BDD_MODEL_ONTRACK_URL} \
     ${SERENITY_OPTIONS} \
     -jar /ontrack/bdd/app/ontrack-bdd-app.jar
