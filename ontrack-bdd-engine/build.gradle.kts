9d0ad2d7cea7889b0c79da3395a79539787e968c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-10 16:50:38 +0100

    [bdd] Cucumber 4

--- ontrack-bdd-engine/build.gradle.kts
+++ ontrack-bdd-engine/build.gradle.kts
@@ -7,7 +7,7 @@ val serenityVersion: String = "2.0.82"
 dependencies {
     api("net.serenity-bdd:serenity-core:${serenityVersion}")
     api("net.serenity-bdd:serenity-spring:${serenityVersion}")
-    api("net.serenity-bdd:serenity-cucumber:1.9.45")
+    api("net.serenity-bdd:serenity-cucumber4:1.0.21")
 
     implementation("junit:junit")
     implementation("org.apache.commons:commons-lang3")
