9c7f298afde52f0a07677584b672280ebf32bc10
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-27 20:41:58 +0100

    [graphql] Project list

--- ontrack-graphql/src/main/kotlin/net/nemerosa/ontrack/ui/graphql/RootQueryGraphQL.kt
+++ ontrack-graphql/src/main/kotlin/net/nemerosa/ontrack/ui/graphql/RootQueryGraphQL.kt
@@ -3,11 +3,13 @@ package net.nemerosa.ontrack.ui.graphql
 import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.TypeRuntimeWiring
 import net.nemerosa.ontrack.model.security.AccountService
+import net.nemerosa.ontrack.model.structure.StructureService
 import org.springframework.stereotype.Component
 
 @Component
 class RootQueryGraphQL(
-        private val accountService: AccountService
+        private val accountService: AccountService,
+        private val structureService: StructureService
 ) : AbstractGraphQLContributor() {
     override fun wire(wiring: RuntimeWiring.Builder) {
         wiring.type(
@@ -19,6 +21,14 @@ class RootQueryGraphQL(
                             val mapping: String? = env.getArgument("mapping")
                             // TODO Filter
                             accountService.accountGroups
+                        }
+                        // TODO Use contributors
+                        .dataFetcher("projects") { env ->
+                            val id: Int? = env.getArgument("id")
+                            val name: String? = env.getArgument("name")
+                            val favourites: Boolean? = env.getArgument("favourites")
+                            // TODO Filter
+                            structureService.projectList
                         }
         )
     }
