81a65d2f07a944ee8f6bf9d8b9031155ad93ebd2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-01-20 21:09:54 +0100

    [kdsl] Runner using script engine

--- ontrack-kdsl-runner/build.gradle.kts
+++ ontrack-kdsl-runner/build.gradle.kts
@@ -4,8 +4,15 @@ plugins {
 
 apply(plugin = "org.springframework.boot")
 
+val kotlinVersion: String by project
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation(project(":ontrack-kdsl-impl"))
     implementation("args4j:args4j")
+
+    implementation(kotlin("script-runtime", kotlinVersion))
+    implementation(kotlin("compiler-embeddable", kotlinVersion))
+    implementation(kotlin("scripting-compiler-embeddable", kotlinVersion))
+    implementation(kotlin("script-util", kotlinVersion))
 }
--- ontrack-kdsl-runner/src/main/kotlin/net/nemerosa/ontrack/kdsl/runner/core/KDSLRunnerImpl.kt
+++ ontrack-kdsl-runner/src/main/kotlin/net/nemerosa/ontrack/kdsl/runner/core/KDSLRunnerImpl.kt
@@ -1,13 +1,30 @@
 package net.nemerosa.ontrack.kdsl.runner.core
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import net.nemerosa.ontrack.kdsl.model.Ontrack
 import net.nemerosa.ontrack.kdsl.runner.KDSLRunner
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
 
-class KDSLRunnerImpl(ontrack: Ontrack) : KDSLRunner {
+class KDSLRunnerImpl(private val ontrack: Ontrack) : KDSLRunner {
 
     override fun kdsl(script: String, bindings: Map<String, Any?>): JsonNode {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Gets the script engine
+        val engine: ScriptEngine = ScriptEngineManager().getEngineByName("kotlin")
+                ?: throw IllegalStateException("Cannot find kotlin script engine")
+        // Bindings
+        val engineBindings = engine.createBindings()
+        engineBindings.putAll(bindings)
+        engineBindings["ontrack"] = ontrack
+        // Evaluation
+        val result = engine.eval(script, engineBindings)
+        // Result transformation if needed
+        return if (result is JsonNode) {
+            result
+        } else {
+            ObjectMapper().valueToTree<JsonNode>(result)
+        }
     }
 
 }
